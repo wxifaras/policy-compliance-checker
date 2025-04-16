55863b39df293fc5306da718fb5dcdcbde5f32ac
William Xifaras <wxifaras@outlook.com>
2025-04-16 09:04:13 -0400

    minor

--- PolicyComplianceCheckerApi/Models/PolicyCheckerResults.cs
+++ PolicyComplianceCheckerApi/Models/PolicyCheckerResults.cs
@@ -1,11 +1,10 @@
-﻿namespace PolicyComplianceCheckerApi.Models
+﻿namespace PolicyComplianceCheckerApi.Models;
+
+public class PolicyCheckerResult
 {
-    public class PolicyCheckerResult
-    {
-        public string ViolationsSasUri { get; set; }
-        public int ViolationsCount { get; set; }
-        public string EngagementLetterName { get; set; }
-        public string PolicyFileName { get; set; }
-        public string PolicyVersion { get; set; }
-    }
+    public string ViolationsSasUri { get; set; }
+    public int ViolationsCount { get; set; }
+    public string EngagementLetterName { get; set; }
+    public string PolicyFileName { get; set; }
+    public string PolicyVersion { get; set; }
 }
